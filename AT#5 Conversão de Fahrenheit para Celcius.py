bbc1ae5e6aea252966996ec198af77dbc61fd869
MasterM4ts <129786740+MasterM4ts@users.noreply.github.com>
2023-05-08 14:22:43 -0400

    Update add AT #5 it is 6.
    add AT #5 it is 6. (08/05/2023)

---
+++ AT#5 Conversão de Fahrenheit para Celcius.py
@@ -0,0 +1,11 @@
+#Calculo para converter Fahrenheit em Celcius.
+print("+"+"="*52+"+")
+print(" "*2 + "<Conversor de Temperatura Fahrenheit para Celcius>")
+print("+"+"="*52+"+")
+fah = float(input("Digite o Valor Que Deseja Converter: "))
+
+c = (5/9) * (fah -32)
+
+print("+"+"="*52+"+")
+print("O Resultado da Conversão foi %.2f C."%fah)
+print("+"+"="*52+"+")
---
+++ AT#6 Estoque de Produtos.py
@@ -0,0 +1,18 @@
+#Sistema para ler Nome do Produto,Quantidade Comprada,o Valor Unitário e o Porcentual de Desconto.
+#Após ler ,Calcular o Valor Total. 
+print("-"*30)
+name_Produto = input("Digite o Nome do Produto: \n>> ")
+quantidade_Produto = int(input("Digite a Quantidade de Produtos: \n>> "))
+valor_Uni = float(input("Digite o Valor Unitário Do Produto: \n>> "))
+desconto_Produto = float(input("Digite o Porcentual de Desconto do Produto: \n>> "))
+
+valor_Total = quantidade_Produto * valor_Uni
+valor_Total = valor_Total - desconto_Produto
+
+print("+"+"="*26+"+")
+print(" "*7 + "<Nota Fiscal>")
+print("-"*27)
+print("i| Nome do Produto: %s."%name_Produto)
+print("-"*27)
+print("i| Valor Total: R$%.2f"%valor_Total)
+print("+"+"="*26+"+")
