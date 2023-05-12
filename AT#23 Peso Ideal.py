49278be3c1fd95c4ff6b7fca0ce0f4a386395e96
MasterM4ts <masterm4ts@gmail.com>
2023-05-12 17:32:10 -0400

    Add added new notes and AT#21,22 It is 23
    Add (12/05/2023).

---
+++ AT#23 Peso Ideal.py
@@ -0,0 +1,9 @@
+#Programa que calcula Peso Ideal.
+print("+"+"="*35+"+")
+altura = float(input("Digite sua Altura: \n>> "))
+
+peso_Ideal = (72.7*altura) - 58
+
+print("+"+"="*42+"+")
+print("I| Peso Ideal para a Altura %.2f: >> %.2f"%(altura,peso_Ideal))
+print("+"+"="*42+"+")
