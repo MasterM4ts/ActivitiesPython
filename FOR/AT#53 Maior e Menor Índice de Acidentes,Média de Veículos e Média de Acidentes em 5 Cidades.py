d161c4adb9717d72c887af460ab813dafea96781
MasterM4ts <masterm4ts@gmail.com>
2023-06-02 17:24:35 -0400

    Update #AT 51 to 55.
    Add (02/06/2023).

--- FOR/AT#53 Maior e Menor Índice de Acidentes,Média de Veículos e Média de Acidentes em 5 Cidades.py
+++ FOR/AT#53 Maior e Menor Índice de Acidentes,Média de Veículos e Média de Acidentes em 5 Cidades.py
@@ -1,3 +1,4 @@
+#Programa que Informa o Maior e Menor Índice de Acidentes,Média de Veículos e Média de Acidentes em 5 Cidades.
 
 maior = -1
 menor = 9999999999
@@ -18,7 +19,7 @@ for i in range(5):
 
     soma_V += quant_veiculos
     
-    if soma_V < 2000:
+    if quant_acidentes < 2000:
         soma_A2000 + quant_acidentes
         cont += 1
 
@@ -26,7 +27,7 @@ media = soma_V / 5
 media2000 = soma_V / cont
 
 print("+"+"="*35+"+")
-print("I| Maior e Menor Índice: %")
-print("I| Média de Veiculos nas 5 Cidades: ")
-print("I| Media de Acidentes nas Cidades com Menos de 2000 Veículos: ")
+print("I| Maior e Menor Índice %d e %d, Cidades que Pertence: %d e %d"%(maior,menor,cidade_maior,cidade_menor))
+print("I| Média de Veiculos nas 5 Cidades: %d"%media)
+print("I| Media de Acidentes nas Cidades com Menos de 2000 Veículos: %d"%media2000)
 print("+"+"="*35+"+")
