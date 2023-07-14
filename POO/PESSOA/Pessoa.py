bbfcc6d8903f3cb351a413bff8b48aee87cf57ba
MasterM4ts <masterm4ts@gmail.com>
2023-07-14 17:28:15 -0400

    Updated POO folder with folder and files
    Added (14/07/2023).

---
+++ POO/PESSOA/Pessoa.py
@@ -0,0 +1,17 @@
+class Pessoa:
+
+
+    def __init__(self,nome,idade,endereco,cidade,estado):
+        self.nome = nome
+        self.idade =idade
+        self.endereco = endereco
+        self.cidade = cidade
+        self.estado = estado
+
+    
+    def relatorio(self):
+        print(f"I| Nome...{self.nome}")
+        print(f"I| Idade...{self.idade}")
+        print(f"I| Endereço...{self.endereco}")
+        print(f"I| Cidade...{self.cidade}")
+        print(f"I| Estado...{self.estado}")
---
+++ POO/PESSOA/main.py
@@ -0,0 +1,5 @@
+from Aluno import*
+from Professor import*
+
+y = Professor(1800.00,"Ederson",30,"Senac Hub Academy","Campo Grande","Mato Grosso do Sul")
+x = Aluno(250.00,"Matheus",17,"Jeronimo de Albuquerque,1457","Campo Grande","Mato Grosso do Sul")
