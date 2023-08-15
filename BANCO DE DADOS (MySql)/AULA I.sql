147aadc93f4d7ab381712e413f1210d6c716dfbf
MasterM4ts <masterm4ts@gmail.com>
2023-08-15 16:51:30 -0400

    Data Bank Class.
    A database class was held, thus adding a script with the first MySql commands.
    
    Added (15/08/2023).

---
+++ BANCO DE DADOS (MySql)/AULA I.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE Senac;
+CREATE DATABASE Professor;
+CREATE DATABASE Aluno;
+CREATE DATABASE Cordenação;
+CREATE DATABASE Fabrica;
+
+use	 Senac;		
+
+CREATE TABLE Funcionario(cod_func INTEGER);	
+CREATE TABLE Aluno(id int, nome varchar (30) not null, fone varchar (30) not null, email varchar (30) not null);
+show Tables;			
