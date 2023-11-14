1bb88fe35030a0cb555226be7ccb254de957f1a0
jotafreitas <129786740+MasterM4ts@users.noreply.github.com>
2023-11-14 17:37:29 -0400

    Updated Registration System
    Added (14/11/2023).

--- SISTEMA DE CADASTRO/Sistema.py
+++ SISTEMA DE CADASTRO/Sistema.py
@@ -1,13 +1,37 @@
-import mysql.connector
+from kivymd.app import MDApp
+from kivy.lang import Builder
 
-con = mysql.connector.connect(
-    host= '10.28.1.195',
-    database = 'AEROPORTO',
-    user = 'suporte',
-    password = 'suporte'
-)
+KV = '''
+ScreenManager:
+    Screen:
+        name: 'screen1'
 
-cursor = con.cursor()
+        MDTopAppBar:
+            title: "Tela 1"
 
+        MDIconButton:
+            icon: 'android'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+            on_release: app.change_screen('screen2')
 
-    
+    Screen:
+        name: 'screen2'
+
+        MDTopAppBar:
+            title: "Tela 2"
+
+        MDIconButton:
+            icon: 'apple'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+            on_release: app.change_screen('screen1')
+'''
+
+class TestApp(MDApp):
+    def build(self):
+        return Builder.load_string(KV)
+
+    def change_screen(self, screen_name):
+        self.root.current = screen_name
+
+TestApp().run()
+
